f20833addd311fb93ae825fe1c55f36229844ca1
MalikAyaan456 <pythoncodetyper@gmail.com>
2026-05-01 15:04:19 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "30px",
+      fontWeight: "bold"
+    }}>
+      Vibe Agent Loading...
+    </div>
+  );
+}
